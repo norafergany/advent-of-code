055a6092c41e4f36cd3b2d5c9fb1416eee0f005f
Nora Fergany <nfergany@andrew.cmu.edu>
2022-12-04 23:46:23 -0500

    Remove global variables

--- day_2/strategy_score.py
+++ day_2/strategy_score.py
@@ -1,60 +1,65 @@
-rock_points = 1
-paper_points = 2
-scissors_points = 3
+class GameRules:
+    def __init__(self):
+        self.rock_points = 1
+        self.paper_points = 2
+        self.scissors_points = 3
 
-win_points = 6
-draw_points = 3
-lose_points = 0
+        self.win_points = 6
+        self.draw_points = 3
+        self.lose_points = 0
 
-rock_dict = {
-    "points": {
-        "X": draw_points + rock_points,
-        "Y": win_points + paper_points,
-        "Z": lose_points + scissors_points,
-    },
-    "strategy": {
-        "X": lose_points + scissors_points,
-        "Y": draw_points + rock_points,
-        "Z": win_points + paper_points
+        self.rock_dict = {
+            "points": {
+                "X": self.draw_points + self.rock_points,
+                "Y": self.win_points + self.paper_points,
+                "Z": self.lose_points + self.scissors_points,
+            },
+            "strategy": {
+                "X": self.lose_points + self.scissors_points,
+                "Y": self.draw_points + self.rock_points,
+                "Z": self.win_points + self.paper_points
 
-    }
+            }
 
-}
+        }
 
-paper_dict = {
-    "points": {
-        "X": lose_points + rock_points,
-        "Y": draw_points + paper_points,
-        "Z": win_points + scissors_points
-    },
-    "strategy": {
-        "X": lose_points + rock_points,
-        "Y": draw_points + paper_points,
-        "Z": win_points + scissors_points
+        self.paper_dict = {
+            "points": {
+                "X": self.lose_points + self.rock_points,
+                "Y": self.draw_points + self.paper_points,
+                "Z": self.win_points + self.scissors_points
+            },
+            "strategy": {
+                "X": self.lose_points + self.rock_points,
+                "Y": self.draw_points + self.paper_points,
+                "Z": self.win_points + self.scissors_points
 
-    }
-}
+            }
+        }
 
-scissors_dict = {
-    "points": {
-        "X": win_points + rock_points,
-        "Y": lose_points + paper_points,
-        "Z": draw_points + scissors_points,
-    },
-    "strategy": {
-        "X": lose_points + paper_points,
-        "Y": draw_points + scissors_points,
-        "Z": win_points + rock_points
+        self.scissors_dict = {
+            "points": {
+                "X": self.win_points + self.rock_points,
+                "Y": self.lose_points + self.paper_points,
+                "Z": self.draw_points + self.scissors_points,
+            },
+            "strategy": {
+                "X": self.lose_points + self.paper_points,
+                "Y": self.draw_points + self.scissors_points,
+                "Z": self.win_points + self.rock_points
 
-    }
-}
+            }
+        }
 
-# The points dictionary to use based on Player 1's move
-player_1_dict = {
-    "A": rock_dict,
-    "B": paper_dict,
-    "C": scissors_dict
-}
+        # The points dictionary to use based on Player 1's move
+        self.player_1_dict = {
+            "A": self.rock_dict,
+            "B": self.paper_dict,
+            "C": self.scissors_dict
+        }
+
+
+game = GameRules()
 
 
 def part_1(file):
@@ -66,7 +71,7 @@ def part_1(file):
             round_points = 0
             curr_round = line.strip().split(' ')
 
-            calc_round = player_1_dict.get(curr_round[0])['points']
+            calc_round = game.player_1_dict.get(curr_round[0])['points']
             player_2 = curr_round[1]
             round_points += calc_round.get(player_2)
 
@@ -83,7 +88,7 @@ def part_2(file):
             round_points = 0
             curr_round = line.strip().split(' ')
 
-            calc_round = player_1_dict.get(curr_round[0])['strategy']
+            calc_round = game.player_1_dict.get(curr_round[0])['strategy']
             player_2 = curr_round[1]
             round_points += calc_round.get(player_2)
 
